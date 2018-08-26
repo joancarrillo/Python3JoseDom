7c99114ff4fdbae1eed9ec7c996bff15886d8784
josedom24 <josedom24@gmail.com>
2018-08-26 18:47:30 +0200

    cadenas

--- ejercicios/cadenas/ejercicio10.py
+++ ejercicios/cadenas/ejercicio10.py
@@ -2,7 +2,7 @@
 # palíndroma es aquella que se lee igual adelante que atrás.
 
 cad = input("Introduce una cadena:")
-if cad == cad[::-1]:
+if cad.lower() == cad[::-1].lower():
 	print("Es un palíndromo")
 else:
 	print("No es un palíndromo")
