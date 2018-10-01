679d52cb0a413bc1d981b02c28e2272a4f3bf05f
josedom24 <josedom24@gmail.com>
2018-10-01 22:20:30 +0200

    ejercicios objetos

--- ejercicios/objetos/ejercicio2.py
+++ ejercicios/objetos/ejercicio2.py
@@ -1,9 +1,8 @@
-from ejercicio1 import Persona
 class Cuenta():
 
     def __init__(self,titular,cantidad=0):
         self.titular=titular
-        self.cantidad=cantidad
+        self.__cantidad=cantidad
     
     @property
     def titular(self):
@@ -17,21 +16,15 @@ class Cuenta():
     def titular(self,titular):
         self.__titular=titular
 
-    @cantidad.setter
-    def cantidad(self,cantidad):
-        if cantidad<0:
-            self.__cantidad=0
-        else:    
-            self.__cantidad=cantidad
-    
+       
     def mostrar(self):
         return "Cuenta\n"+"Titular:"+self.__titular.mostrar()+" - Cantidad:"+str(self.cantidad)
     
     def ingresar(self,cantidad):
         if cantidad > 0:
-            self.cantidad = self.cantidad + cantidad
+            self.__cantidad = self.__cantidad + cantidad
     
     def retirar(self,cantidad):
         if cantidad > 0:
-            self.cantidad = self.cantidad - cantidad
+            self.__cantidad = self.__cantidad - cantidad
     
