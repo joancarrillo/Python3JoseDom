91db50dfac7e6b64c9b2586862e279a116c5ad60
José Domingo Muñoz Rodríguez <josedom24@gmail.com>
2019-05-07 10:34:58 +0000

    Update ejercicios/secuencial/ejercicio16.py

--- ejercicios/secuencial/ejercicio16.py
+++ ejercicios/secuencial/ejercicio16.py
@@ -8,6 +8,6 @@
 velocidad1 = float(input("Dime la velocidad del coche 1 (km/h):"))
 velocidad2 = float(input("Dime la velocidad del coche 2 (km/h):"))
 distancia = float(input("Dime la distancia entre los coches (km):"))
-tiempo = distancia / (velocidad1 - velocidad2)
+tiempo = distancia / abs(velocidad1 - velocidad2)
 tiempo = tiempo * 60
 print("Lo alcanza en",tiempo,"minutos.")
